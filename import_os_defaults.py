edd9f14d30d3bb11f3cb3a8fdfd6f29b12c5ee6a
Brian Kellogg <flakes@Brians-Mac-mini.local>
2021-01-02 10:17:25 -0500

    Adding windows PS directory/file metadata harvesting script, adding initial DB design as I think the best method for storing and referencing this data is SqlLite. Adding start of Python script to update the DB with the metadata.

--- import_os_defaults.py
+++ import_os_defaults.py
@@ -3,6 +3,9 @@
 import sqlite3
 import json
 
+sqlite3.connect('OS_defaults.db')
+
+# have to specify encoding, need to handle this more elegantly for other OS'
 with open("md.json", 'r', encoding='utf-16') as f:
     for line in f:
         record = json.loads(line)
