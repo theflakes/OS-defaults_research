a30e24b4ecc08a59c09f40ba757775eb439b6039
Brian Kellogg <flakes@Brians-Mac-mini.local>
2021-01-03 09:53:31 -0500

    Finishing Python script for DB import, untested at this point.

--- import_os_defaults.py
+++ import_os_defaults.py
@@ -3,6 +3,28 @@
 import sqlite3
 import json
 
+
+def get_os_directory_item_ids(c, os, path, name):
+    c.execute("SELECT id FROM OS WHERE version=?", (os,))
+    o_id = c.fetchone()[0]
+    c.execute("SELECT id FROM Directory WHERE path=?;", (path,))
+    d_id = c.fetchone()[0]
+    c.execute("SELECT id FROM Item WHERE name=?;", (name,))
+    i_id = c.fetchone()[0]
+    return o_id, d_id, i_id
+
+def get_os_directory_item_id(c, o_id, d_id, i_id):
+    c.execute("SELECT id FROM OS_Directory_Item WHERE os_id=? AND directory_id=? AND item_id=?;", 
+                    (
+                        o_id, 
+                        d_id, 
+                        i_id
+                    )
+                )
+    odi_id = c.fetchone()[0]
+    return odi_id
+
+
 db = sqlite3.connect('OS_defaults.db')
 c = db.cursor()
 
@@ -23,12 +45,7 @@ with open('md.json', 'r', encoding='utf-16') as f:
                     )
                 )
 
-        c.execute("SELECT id FROM OS WHERE version='10 Pro N'")
-        o_id = c.fetchone()[0]
-        c.execute("SELECT id FROM Directory WHERE path=?;", (record['ParentPath'],))
-        d_id = c.fetchone()[0]
-        c.execute("SELECT id FROM Item WHERE name=?;", (record['Name'],))
-        i_id = c.fetchone()[0]
+        o_id, d_id, i_id = get_os_directory_item_ids(c, '10 Pro N', record['ParentPath'], record['Name'])
         c.execute("INSERT OR REPLACE INTO OS_Directory_Item (os_id, directory_id, item_id, mode, size, link_type) VALUES(?, ?, ?, ?, ?, ?);", 
                     (
                         o_id,
@@ -39,6 +56,53 @@ with open('md.json', 'r', encoding='utf-16') as f:
                         record['LinkType']
                     )
                 )
+
+        for stream in record['Streams']:
+            c.execute("INSERT OR REPLACE INTO ADS (name) VALUES(?);", 
+                    (
+                        stream['Name'],
+                    )
+                )
+            c.execute("SELECT id FROM OS_Directory_Item WHERE os_id=? AND directory_id=? AND item_id=i_id;", 
+                        (
+                            o_id, 
+                            d_id, 
+                            i_id
+                        )
+                    )
+            odi_id = c.fetchone()[0]
+            c.execute("SELECT id FROM ADS WHERE name=?;", (record['Name'],))
+            a_id = c.fetchone()[0]
+            c.execute("INSERT OR REPLACE INTO Item_ADS (os_directory_item_id, ads_id, size) VALUES(?, ?, ?);", 
+                        (
+                            odi_id,
+                            a_id,
+                            stream['Size']
+                        )
+                    )
+
+    db.commit()
+
+    # We need to loop through it all again to add Link info
+    for line in f:    
+        record = json.loads(line)
+        if record['LinkType']:
+            for link in record['LinkType']:
+                # Get the Links IDs
+                o_id, d_id, i_id = get_os_directory_item_ids(c, '10 Pro N', record['ParentPath'], record['Name'])
+                link_id = get_os_directory_item_id(o_id, d_id, i_id)
+
+                # Get the Links target IDs
+                path_item = link.rsplit('\\', 1)
+                o_id, d_id, i_id = get_os_directory_item_ids(c, '10 Pro N', path_item[0], path_item[1])
+                target_id = get_os_directory_item_id(o_id, d_id, i_id)
+
+                c.execute("INSERT OR REPLACE INTO Item_ADS (link_id, target_id) VALUES(?, ?);", 
+                        (
+                            link_id,
+                            target_id
+                        )
+                    )
 
 db.commit()
 db.close()
