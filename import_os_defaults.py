fd9c0680d574ae54ad1bbe284f589e51cb7a9783
Brian Kellogg <flakes@Brians-Mac-mini.local>
2021-01-02 19:39:25 -0500

    building out DB Python import

--- import_os_defaults.py
+++ import_os_defaults.py
@@ -10,16 +10,35 @@ c = db.cursor()
 with open('md.json', 'r', encoding='utf-16') as f:
     for line in f:
         record = json.loads(line)
-        c.execute("INSERT OR IGNORE INTO Directory (path) VALUES(?)", 
+        c.execute("INSERT OR REPLACE INTO Directory (path) VALUES(?);", 
                     (
-                        record['ParentPath'],)
+                        record['ParentPath'],
                     )
-        c.execute("INSERT OR IGNORE INTO Item (name, base_name, extension) VALUES(?, ?, ?)", 
+                )
+        c.execute("INSERT OR REPLACE INTO Item (name, base_name, extension) VALUES(?, ?, ?);", 
                     (
                         record['Name'],
                         record["BaseName"],
-                        record['Extension'])
+                        record['Extension']
                     )
+                )
+
+        c.execute("SELECT id FROM OS WHERE version='10 Pro N'")
+        o_id = c.fetchone()[0]
+        c.execute("SELECT id FROM Directory WHERE path=?;", (record['ParentPath'],))
+        d_id = c.fetchone()[0]
+        c.execute("SELECT id FROM Item WHERE name=?;", (record['Name'],))
+        i_id = c.fetchone()[0]
+        c.execute("INSERT OR REPLACE INTO OS_Directory_Item (os_id, directory_id, item_id, mode, size, link_type) VALUES(?, ?, ?, ?, ?, ?);", 
+                    (
+                        o_id,
+                        d_id,
+                        i_id,
+                        record['Mode'],
+                        record['Size'],
+                        record['LinkType']
+                    )
+                )
 
 db.commit()
 db.close()
