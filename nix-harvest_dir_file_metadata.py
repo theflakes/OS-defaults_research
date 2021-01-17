ecdc8bd1de80f39f0e63a230a267764ea5079608
theflakes <theflakes@gmail.com>
2021-01-17 10:10:40 -0500

    Building out Python script for harvesting Mac and Linux defaults. Added 'Hidden' field to track that seperately.

--- nix-harvest_dir_file_metadata.py
+++ nix-harvest_dir_file_metadata.py
@@ -1,7 +1,6 @@
-#!/usr/bin/python
+#!/usr/bin/python3
 
-import os
-import sys
+import os, sys, stat
 import getopt
 import json
 
@@ -9,52 +8,6 @@ import json
 recurse = False
 pretty = False
 directory = '/'
-
-# reference: https://gist.github.com/beugley/47b4812df0837fc90e783347faee2432
-def perm_to_text(octal):
-    result =  ''
-    first = 0
-    octal = str(octal)
-    
-    #if there are more than 4 digits, just take the last 4
-    if len(octal)>4:
-        #separate initial digit
-        octal = octal [-4:]
-    #if there are 4 digits, deal with first (setuid, setgid, and sticky flags) separately
-    if len(octal)==4:
-        if octal[0]!='0': 
-            first = int(octal [:1])
-        octal = octal [-3:]
-    value_letters = [(4, 'r'), (2, 'w'), (1, 'x')]
-    # Iterate over each of the digits in octal
-    for permission in [int(n) for n in octal]:
-        # Check for each of the permissions values
-        for value, letter in value_letters:
-            if permission >= value:
-                result += letter
-                permission -= value
-            else:
-                result += '-'
-    if first!=0:
-        for value in [4,2,1]:
-            if first >= value:
-                if value==4:
-                    if result[2] == 'x':
-                        result = result[:2]+'s'+result[3:]
-                    elif result[2] == '-':
-                        result = result[:2]+'S'+result[3:]
-                if value==2:
-                    if result[5] == 'x':
-                        result = result[:5]+'s'+result[6:]
-                    elif result[5] == '-':
-                        result = result[:5]+'S'+result[6:]            
-                if value==1:
-                    if result[8] == 'x':
-                        result = result[:8]+'t'+result[9:]
-                    elif result[8] == '-':
-                        result = result[:8]+'T'+result[9:]             
-                first -= value   
-    return result
 
 
 def print_log(log):
@@ -73,7 +26,7 @@ def get_metadata(parent_dir, path, item):
         else:
             extension = None
         md = os.stat(path)
-        mode = perm_to_text(md.st_mode)
+        mode = stat.filemode(md.st_mode)
         log['Extension'] = extension
         log['Mode'] = mode
         log['ParentPath'] = parent_dir
