b7324e9662850283e1cb6a8422ec82dcc482d51a
Brian Kellogg <flakes@gmail.com>
2022-07-30 10:34:39 -0400

    retiring sqlite db for storage

---
+++ retired/build_db.sql
@@ -0,0 +1,106 @@
+CREATE TABLE IF NOT EXISTS "Arch" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"arch"	TEXT NOT NULL,
+	"bits"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE sqlite_sequence(name,seq);
+CREATE TABLE IF NOT EXISTS "ADS" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "Directory" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"path"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "Item" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	"base_name"	TEXT,
+	"extension"	TEXT,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "Item_ADS" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"os_directory_item_id"	INTEGER NOT NULL,
+	"ads_id"	INTEGER NOT NULL,
+	"size"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("os_directory_item_id") REFERENCES "OS_Directory_Item_Hash"("id"),
+	FOREIGN KEY("ads_id") REFERENCES "ADS"("id")
+);
+CREATE TABLE IF NOT EXISTS "Link" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"link_id"	INTEGER NOT NULL,
+	"link_target_id"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("link_id") REFERENCES "OS_Directory_Item_Hash"("id")
+);
+CREATE TABLE IF NOT EXISTS "OS" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"os"	TEXT NOT NULL UNIQUE,
+	"version"	TEXT NOT NULL UNIQUE,
+	"arch_id"	INTEGER NOT NULL,
+	PRIMARY KEY("id" AUTOINCREMENT),
+	FOREIGN KEY("arch_id") REFERENCES "Arch"("id")
+);
+CREATE TABLE IF NOT EXISTS "Hash" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"md5"	TEXT NOT NULL UNIQUE,
+	"sha1"	TEXT NOT NULL UNIQUE,
+	"sha256"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "Group_User" (
+	"id"	INTEGER UNIQUE,
+	"name"	TEXT NOT NULL UNIQUE,
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
+CREATE TABLE IF NOT EXISTS "OS_Directory_Item_Hash" (
+	"id"	INTEGER NOT NULL UNIQUE,
+	"os_id"	INTEGER NOT NULL,
+	"directory_id"	INTEGER NOT NULL,
+	"item_id"	INTEGER NOT NULL,
+	"hash_id"	INTEGER,
+	"group_id"	INTEGER,
+	"user_id"	INTEGER,
+	"mode"	TEXT NOT NULL,
+	"size"	INTEGER NOT NULL,
+	"is_link"	INTEGER,
+	"is_hidden"	INTEGER NOT NULL,
+	"comments"	TEXT,
+	"company_name"	TEXT,
+	"file_build_part"	INTEGER,
+	"file_description"	TEXT,
+	"file_major_part"	INTEGER,
+	"file_minor_part"	INTEGER,
+	"filename"	TEXT,
+	"file_private_part"	INTEGER,
+	"file_version"	TEXT,
+	"internal_name"	TEXT,
+	"is_debug"	INTEGER,
+	"is_patched"	INTEGER,
+	"is_private_build"	INTEGER,
+	"is_prerelease"	INTEGER,
+	"is_special_build"	INTEGER,
+	"language"	TEXT,
+	"legal_copyright"	TEXT,
+	"legal_trademarks"	TEXT,
+	"original_filename"	TEXT,
+	"private_build"	TEXT,
+	"product_build_part"	INTEGER,
+	"product_major_part"	INTEGER,
+	"product_minor_part"	INTEGER,
+	"product_name"	TEXT,
+	"product_private_part"	INTEGER,
+	"product_version"	TEXT,
+	FOREIGN KEY("os_id") REFERENCES "OS"("id"),
+	FOREIGN KEY("user_id") REFERENCES "Group_User"("id"),
+	FOREIGN KEY("hash_id") REFERENCES "Hash"("id"),
+	FOREIGN KEY("item_id") REFERENCES "Item"("id"),
+	FOREIGN KEY("directory_id") REFERENCES "Directory"("id"),
+	FOREIGN KEY("group_id") REFERENCES "Group_User"("id"),
+	PRIMARY KEY("id" AUTOINCREMENT)
+);
---
+++ retired/import_os_defaults.py
@@ -0,0 +1,212 @@
+#!/usr/bin/python3
+
+import sqlite3
+import json
+
+
+def get_os_directory_item_hash_ids(c, os, path, name, sha256, group, user):
+    c.execute("SELECT id FROM OS WHERE version=?", (os,))
+    o_id = c.fetchone()[0]
+    c.execute("SELECT id FROM Directory WHERE path=?;", (path,))
+    d_id = c.fetchone()[0]
+    c.execute("SELECT id FROM Item WHERE name=?;", (name,))
+    i_id = c.fetchone()[0]
+    c.execute("SELECT id FROM Hash WHERE sha256=?;", (sha256,))
+    h_id = c.fetchone()[0]
+    c.execute("SELECT id FROM Group_User WHERE name=?;", (group,))
+    g_id = c.fetchone()[0]
+    c.execute("SELECT id FROM Group_User WHERE name=?;", (user,))
+    u_id = c.fetchone()[0]
+    return o_id, d_id, i_id, h_id, g_id, u_id
+
+
+def get_os_directory_item_hash_id(c, o_id, d_id, i_id, h_id, g_id, u_id):
+    c.execute("SELECT id FROM OS_Directory_Item_Hash WHERE os_id=?"                 +
+                                                            "AND directory_id=?"    +
+                                                            "AND item_id=?"         +
+                                                            "AND hash_id=?"         +
+                                                            "AND group_id=?"        +
+                                                            "AND user_id=?;", 
+                    (
+                        o_id, 
+                        d_id, 
+                        i_id,
+                        h_id,
+                        g_id,
+                        u_id
+                    )
+                )
+    odi_id = c.fetchone()[0]
+    return odi_id
+
+
+db = sqlite3.connect('os_defaults.db')
+c = db.cursor()
+
+# have to specify encoding, may need to handle this more elegantly for other OS'
+with open('md.json', 'r', encoding='utf-16') as f:
+    for line in f:
+        record = json.loads(line)
+        if record['DataType'] == "FileSystem":
+            c.execute("INSERT OR REPLACE INTO Directory (path) VALUES(?);", 
+                        (
+                            record['ParentPath'],
+                        )
+                    )
+            c.execute("INSERT OR REPLACE INTO Item (name, base_name, extension) VALUES(?, ?, ?);", 
+                        (
+                            record['Name'],
+                            record["BaseName"],
+                            record['Extension']
+                        )
+                    )
+            c.execute("INSERT OR REPLACE INTO Hash (md5, sha1, sha256) VALUES(?, ?, ?);", 
+                        (
+                            record['md5'],
+                            record["sha1"],
+                            record['sha256']
+                        )
+                    )
+
+            c.execute("INSERT OR REPLACE INTO Group_User (Name) VALUES(?);", 
+                        (
+                            record['Group'],
+                        )
+                    )
+
+            c.execute("INSERT OR REPLACE INTO Group_User (Name) VALUES(?);", 
+                        (
+                            record['User'],
+                        )
+                    )
+
+            o_id, d_id, i_id, h_id, g_id, u_id = get_os_directory_item_hash_ids(c, record['OS'], record['ParentPath'], 
+                                                                                record['Name'], record['sha256'], 
+                                                                                record['Group'], record['User'])
+            c.execute("INSERT OR REPLACE INTO OS_Directory_Item_Hash (" +
+                            "os_id,"                    +
+                            "directory_id,"             +
+                            "item_id,"                  +
+                            "hash_id,"                  +
+                            "g_id"                      +
+                            "u_id"                      +
+                            "mode,"                     +
+                            "size,"                     +
+                            "is_link,"                  +
+                            "is_hidden,"                +
+                            "comments,"                 +
+                            "company_name,"             +
+                            "file_build_part,"          +
+                            "file_description,"         +
+                            "file_major_part,"          +
+                            "file_minor_part,"          +
+                            "filename,"                 +
+                            "file_private_part,"        +
+                            "file_version,"             +
+                            "internal_name,"            +
+                            "is_debug,"                 +
+                            "is_patched,"               +
+                            "is_private_build,"         +
+                            "is_prerelease,"            +
+                            "is_special_build,"         +
+                            "language,"                 +
+                            "legal_copyright,"          +
+                            "legal_trademarks,"         +
+                            "original_filename,"        +
+                            "private_build,"            +
+                            "product_build_part,"       +
+                            "product_major_part,"       +
+                            "product_minor_part,"       +
+                            "product_name,"             +
+                            "product_private_part,"     +
+                            "product_version,"          +
+                        "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?);", 
+                        (
+                            o_id,
+                            d_id,
+                            i_id,
+                            h_id,
+                            g_id,
+                            u_id,
+                            record["Mode,"],
+                            record["Size,"],
+                            record["Link,"],
+                            record["Hidden,"],
+                            record["Comments,"],
+                            record["CompanyName,"],
+                            record["FileBuildPart,"],
+                            record["FileDescription,"],
+                            record["FileMajorPart,"],
+                            record["FileMinorPart,"],
+                            record["FileName,"],
+                            record["FilePrivatePart,"],
+                            record["FileVersion,"],
+                            record["InternalName,"],
+                            record["IsDebug,"],
+                            record["IsPatched,"],
+                            record["IsPrivateBuild,"],
+                            record["IsPreRelease,"],
+                            record["IsSpecialBuild,"],
+                            record["Language,"],
+                            record["LegalCopyright,"],
+                            record["LegalTrademarks,"],
+                            record["OriginalFilename,"],
+                            record["PrivateBuild,"],
+                            record["ProductBuildPart,"],
+                            record["ProductMajorPart,"],
+                            record["ProductMinorPart,"],
+                            record["ProductName,"],
+                            record["ProductPrivatePart,"],
+                            record["ProductVersion,"]
+                        )
+                    )
+
+            for stream in record['Streams']:
+                c.execute("INSERT OR REPLACE INTO ADS (name) VALUES(?);", 
+                        (
+                            stream['Name'],
+                        )
+                    )
+                c.execute("SELECT id FROM OS_Directory_Item WHERE os_id=? AND directory_id=? AND item_id=i_id;", 
+                            (
+                                o_id, 
+                                d_id, 
+                                i_id
+                            )
+                        )
+                odi_id = c.fetchone()[0]
+                c.execute("SELECT id FROM ADS WHERE name=?;", (record['Name'],))
+                a_id = c.fetchone()[0]
+                c.execute("INSERT OR REPLACE INTO Item_ADS (os_directory_item_id, ads_id, size) VALUES(?, ?, ?);", 
+                            (
+                                odi_id,
+                                a_id,
+                                stream['Size']
+                            )
+                        )
+
+    db.commit()
+
+    # We need to loop through it all again to add Link info
+    for line in f:    
+        record = json.loads(line)
+        if record['LinkType']:
+            for link in record['LinkType']:
+                # Get the Link's IDs
+                o_id, d_id, i_id, h_id = get_os_directory_item_hash_ids(c, record['OS'], record['ParentPath'], record['Name'], record['sha256'])
+                link_id = get_os_directory_item_hash_id(o_id, d_id, i_id, h_id)
+
+                # Get the Link's target IDs
+                path_item = link.rsplit('\\', 1)
+                o_id, d_id, i_id, h_id = get_os_directory_item_hash_ids(c, record['OS'], path_item[0], path_item[1], record['sha256'])
+                target_id = get_os_directory_item_hash_id(o_id, d_id, i_id, h_id)
+
+                c.execute("INSERT OR REPLACE INTO Item_ADS (link_id, target_id) VALUES(?, ?);", 
+                        (
+                            link_id,
+                            target_id
+                        )
+                    )
+
+db.commit()
+db.close()
